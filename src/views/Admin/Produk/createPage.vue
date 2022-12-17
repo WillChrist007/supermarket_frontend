7b136f11a14a86c2e57f432d526b1dcbe7b84d70
Daniel <zikodaniel2@gmail.com>
2022-12-18 00:53:09 +0700

    Tugas besar bagian daniel

--- src/views/Admin/Produk/createPage.vue
+++ src/views/Admin/Produk/createPage.vue
@@ -1,5 +1,147 @@
 <template>
-    <div>
-        
-    </div>
+    <v-main class="list">
+        <v-card>
+            <v-list-item>
+                <v-list-item-avatar color="grey"></v-list-item-avatar>
+                <v-list-item-content>
+                    <v-list-item-title class="headline">Admin</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-card-title>
+                <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Search"
+                    outlined
+                    hidden
+                    details
+                    style="margin-top:30px"
+                ></v-text-field>
+                <v-spacer></v-spacer>
+                <v-btn color="success" dark @click="dialog=true">Tambah</v-btn>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-data-table :headers="headers" :items="todos" :search="search">
+                <template v-slot:[`item.actions`]="{ item }">
+                    <v-btn small class="mr-2 blue-grey lighten-3" @click="editItem(item)">edit</v-btn>
+                    <v-btn small class="mr-2 blue-grey lighten-3" @click="deleteItem(item)">delete</v-btn>
+                </template>
+            </v-data-table>
+        </v-card>
+        <v-dialog v-model="dialog" persistent max-width="600px">
+            <v-card>
+                <v-card-title>
+                    <span class="headline"></span>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-text-field
+                            v-model=""
+                            label="Task"
+                            required
+                        ></v-text-field>
+                        <v-select
+                            v-model=""
+                            :items="[``]"
+                            label=""
+                            required
+                        ></v-select>
+
+                        <v-textarea
+                            v-model=""
+                            label=""
+                            required
+                        ></v-textarea>
+                        <v-select
+                            v-model=""
+                            :items="[``, ``]"
+                            label=""
+                            required
+                        ></v-select>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue-darken-1" text @click="cancel"> Cancel</v-btn>
+                    <v-btn color="blue-darken-1" text @click="save"> Save</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-main>
 </template>
+<script>
+    export default{
+        name: "ListItem",
+        data() {
+            return {
+                search: null,
+                dialog: false,
+                headers: [
+                    {
+                        text: "Task",
+                        align: "start",
+                        sortable: true,
+                        value: "task",
+                    },
+                    { text: "", value: "" },
+                    { text: "", value: "" },
+                    { text: "", value: "" },
+                    { text: "", value: "" },
+                ],
+                todos: [
+                    {
+                        task: "",
+                        priority: "",
+                        note: "",
+                        status: "",
+                    },
+                    {
+                        task: "",
+                        priority: "",
+                        note: "",
+                        status: "",
+                    },
+                    {
+                        task: "",
+                        priority: "",
+                        note: "",
+                        status: "",
+                    },
+                ],
+                formTodo: {
+                    task: null,
+                    priority: null,
+                    note: null,
+                    status: null,
+                },
+            };
+        },
+        methods: {
+            save () {
+                this..push(this.);
+                this.resetForm();
+                this.dialog = false;
+            },
+            cancel() {
+                this.resetForm();
+                this.dialog = false;
+            },
+            resetForm() {
+                this. = {
+                    task: null,
+                    priority: null,
+                    note: null,
+                    status: null,
+                };
+            },
+        },
+    };
+</script>
+<style>
+    .text {
+        font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+        font-size: 40px;
+        font-style: italic;
+    }
+</style>
--- src/views/Admin/Ulasan/indexPage.vue
+++ src/views/Admin/Ulasan/indexPage.vue
@@ -1,5 +1,83 @@
 <template>
-    <div>
-        
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>UPDATE ULASAN</h4>
+                        <hr>
+                        <form @submit.prevent="update">
+                            <div class="form-group mb-3">
+                                <label class="form-label">Ulasan Anda</label>
+                                <b-form-textarea size="lg" type="text" class="form-control" v-model="ulasan.isi" placeholder="Masukkan Ulasan anda disini"></b-form-textarea>
+                                <!-- validation -->
+                                <div v-if="validation.isi" class="mt-2 alert alert-danger">
+                                    {{
+                                            validation.isi[0]
+                                    }}
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">SIMPAN</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import { reactive, ref, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import axios from 'axios'
+export default {
+    setup() {
+        //state departemen
+        const ulasan = reactive({
+            isi : '',
+        })
+        //state validation
+        const validation = ref([])
+        //vue router
+        const router = useRouter()
+
+        const route = useRoute()
+
+        onMounted(() => {
+            //get API from Laravel Backend
+            axios.get(`http://localhost:8000/api/ulasan/${route.params.id}`)
+                .then(response => {
+                    //assign state posts with response data
+                    ulasan.isi = response.data.data.isi
+                }).catch(error => {
+                    console.log(error.response.data)
+                })
+
+        })
+        //method update
+        function update() {
+            let isi = ulasan.isi
+            axios.put(`http://localhost:8000/api/ulasan/${route.params.id}`, {
+                isi: isi,
+            }).then(() => {
+                //redirect ke post index
+                router.push({
+                    name: 'ulasan.index'
+                })
+            }).catch(error => {
+                //assign state validation with error
+                validation.value = error.response.data
+            })
+        }
+        //return
+        return {
+            ulasan,
+            validation,
+            router,
+            update
+        }
+    }
+}
+</script>
+<style>
+
+</style>
