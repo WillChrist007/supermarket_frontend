67e5fe805bc3fb4cba46e70d537bc1d139038dcf
Tenzo <ricardolr088@gmail.com>
2022-12-19 02:13:30 +0700

    Profile

--- src/views/User/Profile/editProfile.vue
+++ src/views/User/Profile/editProfile.vue
@@ -1,123 +1,103 @@
 <template>
-    <div id="app">
-        <b-navbar style="background-color: #1D3557;" type="dark">
-            <b-nav-item style="margin-top:-20px" to="/"><img src="http://tubespawkel10.link/img/logo.png"
-                    style="width: 75%;"></b-nav-item>
-            <b-navbar-nav class="ml-auto" variant="danger">
-                <b-dropdown right>
-                    <template #button-content style="background-color: #1D3557;">
-                        <img :src="url + image" class="mx-auto d-block rounded-circle float-left" alt="Cinque Terre"
-                            width="35px" height="35x">
-                    </template>
-                    <b-dropdown-item to="/indexProfile">Profile</b-dropdown-item>
-                    <b-dropdown-item @click="logout">Log Out</b-dropdown-item>
-                </b-dropdown>
-            </b-navbar-nav>
-
-        </b-navbar>
-
-        <div class="container" style="width: 60%">
-
-            <h1 style="text-align: center; margin: 3rem 0rem; font-weight: bold">UPDATE PROFILE</h1>
-
-            <form action="">
-                <div class="form-group row">
-                    <label for="inputNama" class="col-sm-3 col-form-label">Nama</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control" v-model="nama" readonly>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>PROFILE USER</h4>
+                        <hr>
+                        <form action="">
+                        <div class="form-group mb-3">
+                            <label for="inputNama" class="col-sm-3 col-form-label">Nama</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" v-model="nama">
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="inputNama" class="col-sm-3 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" v-model="email">
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="inputTelp" class="col-sm-3 col-form-label">No. Telp</label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" v-model="telepon">
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="inputAlamat" class="col-sm-3 col-form-label">Alamat</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" v-model="alamat">
+                            </div>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary">SIMPAN</button>
+                    </form>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputTelp" class="col-sm-3 col-form-label">No. Telp</label>
-                    <div class="col-sm-9">
-                        <input type="number" class="form-control" v-model="telepon" readonly>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="inputAlamat" class="col-sm-3 col-form-label">Alamat</label>
-                    <div class="col-sm-9">
-                        <input type="text" class="form-control" v-model="alamat" readonly>
-                    </div>
-                </div>
-
-                <div style="margin: 1rem 0rem;" align="center">
-                    <b-button variant="primary" @click="submit()" class="btn-md center-block mr-5" >Submit</b-button>
-                </div>
-
-            </form>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { useRouter, useRoute } from 'vue-router'
+import { onMounted, ref } from 'vue'
 export default {
-    name: "indexProfile",
-    data() {
+
+    
+    setup() {
+        //reactive state
+        let users = ref([])
+
+        const router = useRouter()
+
+        const route = useRoute();
+
+        const token = localStorage.getItem('token')
+
+        //mounted
+        onMounted(() =>{ 
+            axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+
+            axios.get('http://localhost:8000/api/user')
+            .then(response => {
+                users.value = response.data.data
+                this.nama = response.data.nama
+                this.telepon = response.data.telepon
+                this.alamat = response.data.alamat
+            }).catch(error=> {
+                console.log(error.response.data)
+            })
+        })
+
+        function update() {
+         let toast = useToast()
+         let nama = user.nama
+         let email = user.email
+         axios.
+         put(`http://localhost:8000/api/user/update/${route.params.id}`,{
+             name: nama,
+             email: email,
+         }).then(()=> {
+             toast.success("Cek Inbox anda Untuk verifikasi Email",{
+                 timeout: 2000
+             })
+             router.push({
+                 name: 'login'
+             })
+         }).catch(error=>{
+                 validation.value = error.response.data
+             })
+        }
+
+        //return
         return {
-            nama: '',
-            telepon: '',
-            alamat: '',
-            userID: '',
-            url: this.$image
+            users,
+            update
         }
-    },
-
-    methods: {
-        readData() {
-            var url = this.$api + '/user'
-            this.$http.get(url, {
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('token')
-                }
-            })
-                .then(response => {
-                    this.nama = response.data.name;
-                    this.telepon = response.data.telepon;
-                    this.alamat = response.data.alamat;
-                })
-        },
-        logout() {
-            localStorage.removeItem('id')
-            localStorage.removeItem('token')
-            this.$router.push({
-                name: 'LoginPage'
-            })
-        },
-        submit() {
-            this.load = true;
-                this.$http
-                    .put(this.$api + "/update/" + this.userID, {
-                        name: this.nama,
-                        alamat: this.alamat,
-                        telepon: this.telepon,
-                    })
-                    .then((response) => {
-                        this.error_message = response.data.message;
-                        this.color = "green";
-                        this.snackbar = true;
-                        this.load = false;                     
-                        this.$router.push({
-                            name: "indexProfile",
-                        });
-                    })
-                    .catch((error) => {
-                        this.error_message = error.response.data.message;
-                        this.color = "red";
-                        this.snackbar = true;
-                        this.load = false;
-                    });
-        }
-    },
-
-    mounted() {
-        this.readData();
     }
 }
 </script>
-
-<style>
-@import '../assets/styleToni.css';
-@import url("https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-@import url("https://fonts.googleapis.com/css?family=Sriracha");
-@import url("https://fonts.googleapis.com/css?family=Roboto");
-</style>
--- src/views/User/Profile/profilePage.vue
+++ src/views/User/Profile/profilePage.vue
@@ -1,5 +1,75 @@
 <template>
-    <div>
-        
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 rounded shadow">
+                    <div class="card-body">
+                        <h4>PROFILE USER</h4>
+                        <hr>
+                        <form action="">
+                        <div class="form-group mb-3">
+                            <label for="inputNama" class="col-sm-3 col-form-label">Nama</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" v-model="nama" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="inputNama" class="col-sm-3 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" v-model="email" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="inputTelp" class="col-sm-3 col-form-label">No. Telp</label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" v-model="telepon" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="inputAlamat" class="col-sm-3 col-form-label">Alamat</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" v-model="alamat" readonly>
+                            </div>
+                        </div>
+                        <button to="/editProfile" type="submit" class="btn btn-primary">Change Profile</button>                
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+export default {
+
+    
+    setup() {
+        //reactive state
+        let users = ref([])
+
+        const token = localStorage.getItem('token')
+
+        //mounted
+        onMounted(() =>{ 
+            axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+
+            axios.get('http://localhost:8000/api/user')
+            .then(response => {
+                users.value = response.data.data
+                this.nama = response.data.nama
+                this.telepon = response.data.telepon
+                this.alamat = response.data.alamat
+            }).catch(error=> {
+                console.log(error.response.data)
+            })
+        })
+
+        //return
+        return {
+            users
+        }
+    }
+}
+</script>
